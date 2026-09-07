d14680d0a75da3f9842204c99d08132165cabf1b
aman12-10-12 <aman4483raj@gmail.com>
2026-09-07 12:27:20 +0530

    secure user passwords with bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -11,5 +11,16 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+//Hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//Compare the password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
