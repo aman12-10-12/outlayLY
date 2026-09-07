59693e5053d5c5735f02d42e34b979be586d40c6
aman12-10-12 <aman4483raj@gmail.com>
2026-09-07 23:25:47 +0530

    feat: register authentication routes in server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 
 const app = express();
@@ -20,7 +21,8 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/");
+app.use("/api/v1/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
