18b7b1a27dfb28ed6f0fa612220c67329c5e6db8
aman12-10-12 <aman4483raj@gmail.com>
2026-09-10 01:41:28 +0530

    feat: add dashboard income and expense analytics

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -23,7 +23,29 @@ exports.getDashboardData = async (req, res) => {
       { $group: { _id: null, total: { $sum: "$amount" } } },
     ]);
 
-   
+    //Get income transactions in the last 60 days
+    const last60DaysIncomeTransactions = await Income.find({
+      userId,
+      date: { $gte: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000) },
+    }).sort({ date: -1 });
+
+    //Get total income for last 60 days
+    const incomeLast60Days = last60DaysIncomeTransactions.reduce(
+      (sum, transaction) => sum + transaction.amount,
+      0
+    );
+
+    //Get expense transactions in the last 30 days
+    const last30DaysExpenseTransactions = await Expense.find({
+      userId,
+      date: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
+    }).sort({ date: -1 });
+
+    //Get total income for last 60 days
+    const expensesLast30Days = last30DaysExpenseTransactions.reduce(
+      (sum, transaction) => sum + transaction.amount,
+      0
+    );
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
