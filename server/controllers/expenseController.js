3ba6f69be0139207f607829fd54f99774f4dc20a
aman12-10-12 <aman4483raj@gmail.com>
2026-09-09 09:13:54 +0530

    feat: add expense creation controller

---
+++ server/controllers/expenseController.js
@@ -0,0 +1,34 @@
+const Expense = require("../models/Expense");
+
+//Add Expense source
+exports.addExpense = async (req, res) => {
+  const userId = req.user._id;
+
+  try {
+    const { icon, category, amount, date } = req.body;
+
+    //validation:check for missing fields
+    if (!category || !amount || !date) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const newExpense = new Expense({
+      userId,
+      icon,
+      category,
+      amount,
+      date: new Date(date),
+    });
+
+    await newExpense.save();
+    res.status(200).json(newExpense);
+  } catch (err) {
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+
