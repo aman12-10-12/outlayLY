bc02dd12405d88708f7449b57a77942a69f33df6
aman12-10-12 <aman4483raj@gmail.com>
2026-09-10 20:15:12 +0530

    feat: register dashboard, support and upload API route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
-
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const supportRoutes = require("./routes/supportRoutes");
 
 const app = express();
 
@@ -25,6 +27,11 @@ connectDB();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/support", supportRoutes);
+
+//serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
